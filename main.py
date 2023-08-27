d1d2c6881aaee5ceda60b66d7d1462d8d0d02b18
NiallCarbery <niallcarbery0@gmail.com>
2023-08-27 09:19:18 +0100

    Adds Main() and graphing pf results

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+"""Retirement nest egg calculator using Monte Carlo simulation."""
 import sys
 import random
 import matplotlib.pyplot as plt
@@ -7,13 +8,13 @@ def read_to_list(file_name):
     """Open a file of data in percent, convert to decimal & return a list."""
     with open(file_name) as in_file:
         lines = [float(line.strip()) for line in in_file]
-        decimal = [round(line / 100, 5) for line in in_file]
+        decimal = [round(line / 100, 5) for line in lines]
         return decimal
 
 
 def default_input(prompt, default=None):
-    """Allow use of default values in input."""
-    prompt = '{}[{}]:'.format(prompt, default)
+    """Allow use of default values in input"""
+    prompt = '{} [{}]: '.format(prompt, default)
     response = input(prompt)
     if not response and default:
         return default
@@ -21,8 +22,70 @@ def default_input(prompt, default=None):
         return response
 
 
+# load data files with original data in percent form
+print("\nNote: Input data should be in percent, not decimal!\n")
+try:
+    bonds = read_to_list('10yrUSTBond.txt')
+    stocks = read_to_list('SP500Returns.txt')
+    infl_rate = read_to_list('USInflationRate.txt')
+except IOError as e:
+    print("{}. \nTerminating program.".format(e), file=sys.stderr)
+    sys.exit(1)
+
+# get user input; use dictionary for investment-type argument
+investment_type_args = {'bonds': bonds, 'stocks': stocks,
+                        }
+
+# print input legend for user
+print("   stocks = SP500")
+print("    bonds = 10-yr Treasury Bond")
+
+print("Press ENTER to take default value shown in [brackets]. \n")
+
+# get user input
+invest_type = default_input("Enter investment type: (stocks, bonds,): \n",
+                            'bonds').lower()
+while invest_type not in investment_type_args:
+    invest_type = input("Invalid investment. Enter investment type "
+                        "as listed in prompt: ")
+
+start_value = default_input("Input starting value of investments: \n",
+                            '2000000')
+while not start_value.isdigit():
+    start_value = input("Invalid input! Input integer only: ")
+
+withdrawal = default_input("Input annual pre-tax withdrawal"
+                           " (today's $): \n", '80000')
+while not withdrawal.isdigit():
+    withdrawal = input("Invalid input! Input integer only: ")
+
+min_years = default_input("Input minimum years in retirement: \n", '18')
+while not min_years.isdigit():
+    min_years = input("Invalid input! Input integer only: ")
+
+most_likely_years = default_input("Input most-likely years in retirement: \n",
+                                  '25')
+while not most_likely_years.isdigit():
+    most_likely_years = input("Invalid input! Input integer only: ")
+
+max_years = default_input("Input maximum years in retirement: \n", '40')
+while not max_years.isdigit():
+    max_years = input("Invalid input! Input integer only: ")
+
+num_cases = default_input("Input number of cases to run: \n", '50000')
+while not num_cases.isdigit():
+    num_cases = input("Invalid input! Input integer only: ")
+
+# check for other erroneous input
+if not int(min_years) < int(most_likely_years) < int(max_years) \
+   or int(max_years) > 99:
+    print("\nProblem with input years.", file=sys.stderr)
+    print("Requires Min < ML < Max & Max <= 99.", file=sys.stderr)
+    sys.exit(1)
+
+
 def montecarlo(returns):
-    """Run MCS and return investment at end-of-plan and bankrupt count."""
+    """Run MCS and return investment value and bankrupt count."""
     case_count = 0
     bankrupt_count = 0
     outcome = []
@@ -43,7 +106,7 @@ def montecarlo(returns):
             lifespan_returns.append(returns[i % len(returns)])
             lifespan_infl.append(infl_rate[i % len(infl_rate)])
 
-        # loop through each year
+        # loop through each year of retirement for each case run
         for index, i in enumerate(lifespan_returns):
             infl = lifespan_infl[index]
 
@@ -51,10 +114,10 @@ def montecarlo(returns):
             if index == 0:
                 withdraw_infl_adj = int(withdrawal)
             else:
-                withdraw_infl_adj = int(withdraw_infl_adj * (1 + infl))
+                withdraw_infl_adj = int(withdraw_infl_adj * (1+infl))
 
             investments -= withdraw_infl_adj
-            investments -= int(investments * (1 + i))
+            investments = int(investments * (1 + i))
 
             if investments <= 0:
                 bankrupt = 'yes'
@@ -95,11 +158,10 @@ def main():
     outcome, bankrupt_count = montecarlo(investment_type_args[invest_type])
     odds = bankrupt_prob(outcome, bankrupt_count)
 
+    # generate matplotlib bar chart
     plotdata = outcome[:3000]  # only plot first 3000 runs
-
     plt.figure('Outcome by Case (showing first {} runs)'.format(len(plotdata)),
                figsize=(16, 5))  # size is width, height in inches
-
     index = [i + 1 for i in range(len(plotdata))]
     plt.bar(index, plotdata, color='black')
     plt.xlabel('Simulated Lives', fontsize=18)
@@ -112,72 +174,6 @@ def main():
               fontsize=20, color='red')
     plt.show()
 
-
-# load data files with orginal datat in percent format
-print("\nNote:Input data should be in percent, not in decimal!\n")
-
-try:
-    bonds = read_to_list('10yrUSTBond.txt')
-    stocks = read_to_list('SP500Returns.txt')
-    infl_rate = read_to_list('USInflationRate.txt')
-except IOError as e:
-    print("{}.\nTerminating program.".format(e), file=sys.stderr)
-    sys.exit(1)
-
-# get user input; use dictionary for investment-type arguemnts
-investment_type_args = {'bonds': bonds, 'stocks': stocks}
-
-# print input legend for user
-print(" stocks = S&P500")
-print(" bonds = 10-yr Treasury Bond")
-print("Press ENTER to take default value shown in [brackets]. \n")
-
-# get user input
-invest_type = default_input("Enter investment type: (stocks, bonds): \n",
-                            'bonds').lower()
-while invest_type not in investment_type_args:
-    invest_type = input("Invalid investment. Enter investment type "
-                        "as listed in prompt: ")
-
-
-start_value = default_input("Input starting value of investments: \n",
-                            '2000000')
-while not start_value.isdigit():
-    start_value = input("Invalid input! Input integer only: ")
-
-
-withdrawal = default_input("Input annual pre-tax withdrawal"
-                           " (today's $): \n", '80000')
-while not withdrawal.isdigit():
-    withdrawal = input("Invalid input! Input integer only: ")
-
-
-min_years = default_input("Input minimum years in retirement: \n", '18')
-while not min_years.isdigit():
-    min_years = input("Invalid input! Input integer only: ")
-
-
-most_likely_years = default_input("Input most-likely years in retirement: \n",
-                                  '25')
-while not most_likely_years.isdigit():
-    most_likely_years = input("Invalid input! Input integer only: ")
-
-
-max_years = default_input("Input maximum years in retirement: \n", '40')
-while not max_years.isdigit():
-    max_years = input("Invalid input! Input integer only: ")
-
-
-num_cases = default_input("Input number of cases to run: \n", '50000')
-while not num_cases.isdigit():
-    num_cases = input("Invalid input! Input integer only: ")
-
-# check for erroneous input
-if not int(min_years) < int(most_likely_years) < int(max_years) \
-        or int(max_years) > 99:
-    print("\nProblem with input years,", file=sys.stderr)
-    print("Requires Min < ML < Max with Max <= 99.", file=sys.sterr)
-    sys.exit(1)
 
 # run program
 if __name__ == '__main__':
